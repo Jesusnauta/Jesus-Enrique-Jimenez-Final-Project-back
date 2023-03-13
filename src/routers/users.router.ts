6d71fc26d6ce81bb078a12b5bb86c23aefc616bd
Jesus <jesus.cassano.2006@gmail.com>
2023-03-13 21:21:24 +0100

    🚀 Create user router file

--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,8 +1,15 @@
 import { Router as router } from 'express';
+import { UsersController } from '../controller/users.controller.js';
 import createDebug from 'debug';
+import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
 
-const debug = createDebug('GW:router');
+const debug = createDebug('RM:router');
+
+export const usersRouter = router();
+const repoUsers = UsersMongoRepo.getInstance();
+const controller = new UsersController(repoUsers);
 
 debug('Users Router');
 
-export const usersRouter = router();
+usersRouter.post('/register', controller.register.bind(controller));
+usersRouter.post('/login', controller.login.bind(controller));
