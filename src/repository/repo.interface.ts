586e4a1ea1223cc010032a5da4e9a4101db66aa0
Jesus <jesus.cassano.2006@gmail.com>
2023-03-13 21:21:44 +0100

    ✨ Create repo interface

--- src/repository/repo.interface.ts
+++ src/repository/repo.interface.ts
@@ -1,7 +1,8 @@
 export interface Repo<T> {
   query(): Promise<T[]>;
   queryId(id: string): Promise<T>;
-  create(user: Partial<T>): Promise<T>;
-  update(user: Partial<T>): Promise<T>;
+  create(info: Partial<T>): Promise<T>;
+  update(info: Partial<T>): Promise<T>;
+  search(query: { key: string; value: unknown }): Promise<T[]>;
   destroy(id: string): Promise<void>;
 }
