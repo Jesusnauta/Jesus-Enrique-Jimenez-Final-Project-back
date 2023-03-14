49e422a0a2a1b42c9f7bcc5ab47b085c9ae76cf5
Jesus <jesus.cassano.2006@gmail.com>
2023-03-14 12:06:27 +0100

    🐛 Modify mongo model file

--- src/repository/users.mongo.repo.ts
+++ src/repository/users.mongo.repo.ts
@@ -1,11 +1,11 @@
 import createDebug from 'debug';
-import { UserStructure } from '../entities/users';
+import { Users } from '../entities/users';
 import { HTTPError } from '../errors/errors.js';
 import { Repo } from './repo.interface';
 import { UserModel } from './users.mongo.models.js';
 const debug = createDebug('RM:repo:users');
 
-export class UsersMongoRepo implements Repo<UserStructure> {
+export class UsersMongoRepo implements Repo<Users> {
   private static instance: UsersMongoRepo;
 
   public static getInstance(): UsersMongoRepo {
@@ -20,35 +20,32 @@ export class UsersMongoRepo implements Repo<UserStructure> {
     debug('Instantiate');
   }
 
-  async query(): Promise<UserStructure[]> {
+  async query(): Promise<Users[]> {
     debug('query');
     const data = await UserModel.find().populate('players', { creator: 0 });
     return data;
   }
 
-  async queryId(id: string): Promise<UserStructure> {
+  async queryId(id: string): Promise<Users> {
     debug('queryId');
     const data = await UserModel.findById(id);
     if (!data) throw new HTTPError(404, 'Not found', 'Id not found in queryId');
     return data;
   }
 
-  async search(query: {
-    key: string;
-    value: unknown;
-  }): Promise<UserStructure[]> {
+  async search(query: { key: string; value: unknown }): Promise<Users[]> {
     debug('search');
     const data = await UserModel.find({ [query.key]: query.value });
     return data;
   }
 
-  async create(info: Partial<UserStructure>): Promise<UserStructure> {
+  async create(info: Partial<Users>): Promise<Users> {
     debug('create');
     const data = await UserModel.create(info);
     return data;
   }
 
-  async update(info: Partial<UserStructure>): Promise<UserStructure> {
+  async update(info: Partial<Users>): Promise<Users> {
     debug('update');
     const data = await UserModel.findByIdAndUpdate(info.id, info, {
       new: true,
