58d66df2ccab469abfe3a5d3e519ff5a94df22f6
Jesus <jesus.cassano.2006@gmail.com>
2023-03-14 13:43:00 +0100

    🚀 Add changes in interface repo

--- src/repository/repo.interface.ts
+++ src/repository/repo.interface.ts
@@ -4,5 +4,5 @@ export interface Repo<T> {
   create(info: Partial<T>): Promise<T>;
   update(info: Partial<T>): Promise<T>;
   search(query: { key: string; value: unknown }): Promise<T[]>;
-  destroy(id: string): Promise<void>;
+  delete(id: string): Promise<void>;
 }
