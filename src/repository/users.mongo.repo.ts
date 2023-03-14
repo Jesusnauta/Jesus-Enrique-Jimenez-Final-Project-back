ba4f1535a84a5b903ac91655b53148553c897189
Jesus <jesus.cassano.2006@gmail.com>
2023-03-14 13:43:26 +0100

    🚀 Modify mongo repo

--- src/repository/users.mongo.repo.ts
+++ src/repository/users.mongo.repo.ts
@@ -1,63 +1,62 @@
-import createDebug from 'debug';
 import { Users } from '../entities/users';
 import { HTTPError } from '../errors/errors.js';
 import { Repo } from './repo.interface';
 import { UserModel } from './users.mongo.models.js';
-const debug = createDebug('RM:repo:users');
+import createDebug from 'debug';
+
+const debug = createDebug('GW:users-repo');
 
 export class UsersMongoRepo implements Repo<Users> {
-  private static instance: UsersMongoRepo;
-
-  public static getInstance(): UsersMongoRepo {
-    if (!UsersMongoRepo.instance) {
-      UsersMongoRepo.instance = new UsersMongoRepo();
-    }
-
-    return UsersMongoRepo.instance;
-  }
-
-  private constructor() {
-    debug('Instantiate');
+  constructor() {
+    debug('users-repo-instanced');
   }
 
   async query(): Promise<Users[]> {
-    debug('query');
-    const data = await UserModel.find().populate('players', { creator: 0 });
+    debug('query-method');
+    const data = await UserModel.find().populate('players').exec();
     return data;
   }
 
   async queryId(id: string): Promise<Users> {
-    debug('queryId');
-    const data = await UserModel.findById(id);
-    if (!data) throw new HTTPError(404, 'Not found', 'Id not found in queryId');
+    debug('queryID-method');
+    const data = await UserModel.findById(id).populate('players').exec();
+    if (!data) throw new HTTPError(404, 'Not found', 'ID not found in queryID');
     return data;
   }
 
-  async search(query: { key: string; value: unknown }): Promise<Users[]> {
-    debug('search');
-    const data = await UserModel.find({ [query.key]: query.value });
+  async create(user: Partial<Users>): Promise<Users> {
+    debug('create-method');
+    const data = await UserModel.create(user);
     return data;
   }
 
-  async create(info: Partial<Users>): Promise<Users> {
-    debug('create');
-    const data = await UserModel.create(info);
-    return data;
-  }
-
-  async update(info: Partial<Users>): Promise<Users> {
-    debug('update');
-    const data = await UserModel.findByIdAndUpdate(info.id, info, {
+  async update(user: Partial<Users>): Promise<Users> {
+    debug('update-method');
+    const data = await UserModel.findByIdAndUpdate(user.id, user, {
       new: true,
-    });
-    if (!data) throw new HTTPError(404, 'Not found', 'Id not found in update');
+    })
+      .populate('players')
+      .exec();
+    if (!data) throw new HTTPError(404, 'Not found', 'ID not found in update');
     return data;
   }
 
-  async destroy(id: string): Promise<void> {
-    debug('destroy');
-    const data = await UserModel.findByIdAndDelete(id);
+  async delete(id: string): Promise<void> {
+    debug('delete-method');
+    const data = await UserModel.findByIdAndDelete(id).exec();
     if (!data)
-      throw new HTTPError(404, 'Not found', 'Delete not posible: id not found');
+      throw new HTTPError(
+        404,
+        'Not found',
+        'Delete not possible: ID not found'
+      );
+  }
+
+  async search(query: { key: string; value: unknown }) {
+    debug('search-method');
+    const data = await UserModel.find({ [query.key]: query.value })
+      .populate('players')
+      .exec();
+    return data;
   }
 }
