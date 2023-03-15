03e8afa439148c5f0a6fc369e5ef39842d492334
Jesus <jesus.cassano.2006@gmail.com>
2023-03-15 10:24:30 +0100

    ✅ Add players controller file and test

---
+++ src/controller/players.controller.test.ts
@@ -0,0 +1,99 @@
+import { Response, Request, NextFunction } from 'express';
+import { PlayersController } from './players.controller';
+import { UsersMongoRepo } from '../repository/users.mongo.repo';
+import { PlayersMongoRepo } from '../repository/players.mongo.repo';
+
+describe('Given PlayersController', () => {
+  const repo: PlayersMongoRepo = {
+    create: jest.fn(),
+    query: jest.fn(),
+    search: jest.fn(),
+    queryId: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+  };
+
+  const userRepo = {} as UsersMongoRepo;
+
+  const req = {
+    body: {},
+    params: { id: '' },
+  } as unknown as Request;
+  const resp = {
+    json: jest.fn(),
+  } as unknown as Response;
+  const next = jest.fn();
+
+  const controller = new PlayersController(repo, userRepo);
+
+  describe('When getAll is called', () => {
+    test('Then it should ... if there ara NOT errors', async () => {
+      await controller.getAll(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+
+    test('Then it should ... if there are errors', async () => {
+      (repo.query as jest.Mock).mockRejectedValue(new Error());
+      await controller.getAll(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When get is called', () => {
+    test('Then it should ... if there ara NOT errors', async () => {
+      await controller.get(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+
+    test('Then it should ... if there are errors', async () => {
+      (repo.query as jest.Mock).mockRejectedValue(new Error());
+      await controller.get(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When post is called', () => {
+    test('Then it should ... if there ara NOT errors', async () => {
+      await controller.post(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+
+    test('Then it should ... if there are errors', async () => {
+      (repo.query as jest.Mock).mockRejectedValue(new Error());
+      await controller.post(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When patch is called', () => {
+    test('Then it should ... if there ara NOT errors', async () => {
+      await controller.patch(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+
+    test('Then it should ... if there are errors', async () => {
+      (repo.query as jest.Mock).mockRejectedValue(new Error());
+      await controller.patch(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe('When delete is called', () => {
+    test('Then it should ... if there ara NOT errors', async () => {
+      await controller.delete(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(resp.json).toHaveBeenCalled();
+    });
+
+    test('Then it should ... if there are errors', async () => {
+      (repo.query as jest.Mock).mockRejectedValue(new Error());
+      await controller.delete(req, resp, next);
+      expect(repo.query).toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();
+    });
+  });
+});
