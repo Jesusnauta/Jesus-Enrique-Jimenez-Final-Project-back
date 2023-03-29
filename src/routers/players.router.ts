1ec35a077d6108a26c607b5399e6162039de55f3
Jesus <jesus.cassano.2006@gmail.com>
2023-03-29 18:41:09 +0200

    🐛 Fix players routes files

--- src/routers/players.router.ts
+++ src/routers/players.router.ts
@@ -1,15 +1,35 @@
 import { Router as router } from 'express';
 import { PlayersController } from '../controller/players.controller.js';
-import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
 import createDebug from 'debug';
 import { PlayersMongoRepo } from '../repository/players.mongo.repo.js';
+import { Interceptors } from '../interceptors/interceptors.js';
+import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
+import { authorized } from '../interceptors/authorized.js';
 
 const debug = createDebug('RM:router:players');
 debug('players-router');
 
 export const playersRouter = router();
-const repoPlayers = new PlayersMongoRepo();
-const repoUsers = new UsersMongoRepo();
-const controller = new PlayersController(repoPlayers, repoUsers);
+const playersRepo = new PlayersMongoRepo();
+const usersRepo = new UsersMongoRepo();
+const controller = new PlayersController(playersRepo, usersRepo);
 
-playersRouter.post('/create', controller.post.bind(controller));
+playersRouter.get('/', controller.getAll.bind(controller));
+playersRouter.get('/:id', controller.get.bind(controller));
+playersRouter.post(
+  '/create',
+  Interceptors.logged,
+  controller.create.bind(controller)
+);
+playersRouter.patch(
+  '/edit',
+  Interceptors.logged,
+  controller.patch.bind(controller)
+);
+playersRouter.delete(
+  '/:id',
+  Interceptors.logged,
+  controller.delete.bind(controller)
+);
+// TEMP usersRouter.patch('/addfav/:id', Interceptors.logged, controller.addFav.bind(controller));
+// usersRouter.patch('/deletefav/:id', Interceptors.logged, controller.deleteFav.bind(controller));
